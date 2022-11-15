8b6b1bb1647f560eb0050e605ab196dbdd84e4f5
Will Klein <will@willklein.co>
2022-11-15 13:58:06 -0700

    Setup some simple components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import {
+  PrimaryButton,
+  SecondaryButton,
+  Flex,
+  HStack,
+  AccentIcon,
+} from "@workday/canvas-kit-react";
+
+import SidePanel, {
+  useSidePanel,
+} from "@workday/canvas-kit-preview-react/side-panel";
+
+import {
+  plusIcon,
+  rocketIcon,
+  relatedActionsVerticalIcon,
+  caretDownIcon,
+} from "@workday/canvas-system-icons-web";
+
+const App = () => {
+  const { expanded, panelProps, labelProps, controlProps } = useSidePanel();
+  const [panelState, setPanelState] = React.useState(
+    expanded ? "expanded" : "collapsed"
   );
-}
+  return (
+    <Flex height={320}>
+      <SidePanel {...panelProps} onStateTransition={setPanelState}>
+        <SidePanel.ToggleButton {...controlProps} />
+        {panelState === "expanded" && (
+          <Flex alignItems="center" paddingY="s" paddingX="s">
+            <Flex marginInlineEnd="s">
+              <AccentIcon icon={rocketIcon} />
+            </Flex>
+            <h3 color="licorice500" fontWeight="bold" {...labelProps}>
+              Tasks Panel
+            </h3>
+          </Flex>
+        )}
+      </SidePanel>
+      <Flex
+        as="main"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        flex={1}
+        flexBasis="auto"
+      >
+        <p>Toggle the content direction</p>
+        <SecondaryButton onClick={() => {}} role="button">
+          Second action
+        </SecondaryButton>
+
+        <HStack spacing="s" padding="s">
+          <PrimaryButton>Primary</PrimaryButton>
+          <PrimaryButton icon={plusIcon} iconPosition="start">
+            Primary
+          </PrimaryButton>
+          <PrimaryButton icon={caretDownIcon} iconPosition="end">
+            Primary
+          </PrimaryButton>
+          <PrimaryButton icon={relatedActionsVerticalIcon} />
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default App;
